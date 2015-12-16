e5688909a7fe1c6c27b5d386e146821259bc3111
Arjan de Ruijter <rra@mboutrecht.nl>
2015-12-16 11:35:18 +0100

    weddellzeehond class

---
+++ src/com/gmail/adruijter/WeddellZeehond.java
@@ -0,0 +1,36 @@
+package com.gmail.adruijter;
+
+public class WeddellZeehond extends Zoogdier 
+{
+	// Fields
+	private short duikDiepte;
+	private short tijdZonderZuurstof;
+	private float bovenSnijtandLengte;
+	
+	// Properties
+	//getter
+	public short getDuikDiepte()
+	{
+		return this.duikDiepte;
+	}
+	//setter
+	public void setDuikDiepte(short duikDiepte)
+	{
+		this.duikDiepte = duikDiepte;
+	}
+	
+	//getter
+	public short getTijdZonderZuurstof()
+	{
+		return this.tijdZonderZuurstof;
+	}
+	
+	//setter
+	public void setTijdZonderZuurstof(short tijdZonderZuurstof)
+	{
+		this.tijdZonderZuurstof = tijdZonderZuurstof;
+	}
+	
+	
+
+}
